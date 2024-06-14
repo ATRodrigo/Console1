4a2e54d9e31f6476e60e3ee687bfa07af47bd627
yoshi2218 <rodrigosousaaz18@gmail.com>
2024-06-13 21:21:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Console1 {
+	internal class Program {
+		static void Main(string[] args){
+			Usuario usuario = new Usuario();
+
+			usuario.nome = "Rodrigo";
+			usuario.login = "root";
+			usuario.senha = "qwe123";
+			//-----------
+			string login;
+			string senha;
+
+			do{
+
+			Console.WriteLine("Digite seu login: ");
+			login = Console.ReadLine();
+
+			Console.WriteLine("Digite sua senha: ");
+			senha = Console.ReadLine();
+			}while (!usuario.Logar(login, senha));
+
+			Console.WriteLine(usuario.Logar(login, senha));
+			//Console.WriteLine("Olá, "+usuario.nome);
+			//Console.WriteLine(usuario.Logar("root", "qwe123"));
+
+			//string result = Console.ReadLine();
+			//*Método para ler do teclado 
+			//Console.WriteLine("O usuário digitiou: " + result);
+
+			//usuario.Logar();
+		}
+	}
+}
---
+++ Usuário.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1 {
+	internal class Usuario {
+		//cadeia = strig
+		public string nome;
+		public string login;
+		public string senha;
+		public int qtdLogin;
+		public float peso;
+		public bool ehSolteiro;
+		public char abrvNomeMeio;
+
+
+		public bool Logar(string _login, string _senha) {
+
+			if (_login == login){
+				if (_senha == senha){
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
